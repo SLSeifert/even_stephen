ee049448c4d6b19bde3faf3b2f42fd1ddcf406c8
SLSeifert <slseifert1986@gmail.com>
2025-10-05 15:01:18 -0700

    Connect html, css, and js doc via link and script as well as element ids

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('userNum')
+const output = document.getElementById('output')
+const button = document.getElementById('subButton')
+
+button.addEventListener('click', ()=> {
+    const number = Number(input.value)
+});
