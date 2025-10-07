823385668914d62b63c1062d445d568160170322
SLSeifert <slseifert1986@gmail.com>
2025-10-07 14:41:58 -0700

    Code function to display if input number is even or odd.

--- javascript.js
+++ javascript.js
@@ -4,4 +4,17 @@ const button = document.getElementById('subButton')
 
 button.addEventListener('click', ()=> {
     const number = Number(input.value)
+
+    if (isNaN(number)) {
+        output.textContent = "Enter a valid number.";
+    } else {
+        evenStephen(number);
+    }
 });
+
+function evenStephen(num) {
+    if (num % 2 === 1) {
+        output.textContent = `${num} is odd!`;
+    } else output.textContent = `${num} is even!`;
+}
+
